3bd6709a6e1cfb2acb0e1e46d6143394270a7aac
Chandler <chandlerekins@gmail.com>
2018-05-29 11:31:39 -0700

    Alterations to database scripts

--- JavaCaseStudyGroup/src/interfaces/DatabaseServices.java
+++ JavaCaseStudyGroup/src/interfaces/DatabaseServices.java
@@ -10,4 +10,9 @@ public interface DatabaseServices
 	public void GetAll() throws SQLException, Exception;
 	public void Find() throws SQLException, Exception;
 	
+	//Used to increment char[7] id's used in database
+	public static String incrementID (String id){
+		return String.format("%07d", Integer.parseInt(id)+1);
+	}
+	
 }
